96b4ddbf1491b72c71ecc3a830135a3ad29655e8
valfranciscangeli <vvfranciscangeli@gmail.com>
2024-09-16 14:28:54 -0300

    checkpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
-import Control.Exception (ErrorCall (..), evaluate)
 import T2
-import Test.Hspec (Spec, describe, hspec, it, shouldBe, shouldThrow)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 import Test.QuickCheck (quickCheck)
 
 {-------------------------------------------}
@@ -8,85 +7,79 @@ import Test.QuickCheck (quickCheck)
 {-------------------------------------------}
 
 -- Pruebas para EJERCICIO 1 =============================
-testsEjecicio1 :: Spec
-testsEjecicio1 = describe "EJERCICIO 1" $ do
---   it "pruebas para primosHastaN" $ do
---     primosHastaN 50 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
---     primosHastaN 10 `shouldBe` [2, 3, 5, 7]
---     primosHastaN 1 `shouldBe` []
---     primosHastaN 2 `shouldBe` [2]
 
+-- Parte (b): evalCF
+testsEjecicio1ParteB :: Spec
+testsEjecicio1ParteB = describe "EJERCICIO 1" $ do
+  it "pruebas para 1.b) evalCF" $ do
+    evalCF (Simple 5)  `shouldBe`  (5, 1)
+    evalCF (Compound 1 2 (Simple 3))  `shouldBe`  (5, 3)
+    evalCF t `shouldBe` (649, 200)
 
--- Propiedades para EJERCICIO 1
-propiedadesEjecicio1 :: Spec
-propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
---   it "propiedades de factorizar" $ do
---     quickCheck prop_cantidadFactores
+-- Parte (c): degree
+testsEjecicio1ParteC :: Spec
+testsEjecicio1ParteC = describe "EJERCICIO 1.c) degree" $ do
+  it "pruebas para degree" $ do
+    degree t `shouldBe` 3  -- Ajustar según el grado esperado de `t`
+
+-- Parte (d): foldCF
+testsEjecicio1ParteD :: Spec
+testsEjecicio1ParteD = describe "EJERCICIO 1.d) foldCF" $ do
+  it "pruebas para foldCF" $ do
+    foldCF t id (\a b acc -> a + b + acc) `shouldBe` (3 + 1 + 4 + 1 + 12 + 1 + 4)
+
+-- Parte (e): foldEvalCF y foldDegree
+testsEjecicio1ParteE :: Spec
+testsEjecicio1ParteE = describe "EJERCICIO 1.e) foldEvalCF y foldDegree" $ do
+  it "pruebas para foldEvalCF" $ do
+    foldEvalCF t `shouldBe` evalCF t
+
+  it "pruebas para foldDegree" $ do
+    foldDegree t `shouldBe` degree t
+
+-- Parte (f): evalCF
+testsEjecicio1ParteF :: Spec
+testsEjecicio1ParteF = describe "EJERCICIO 1.f) frac2ConFrac " $ do
+  it "pruebas para frac2ConFrac" $ do
+    frac2ConFrac (649, 200) `shouldBe` t
+
+-- Parte (g): Propiedad
+propiedadEjecicio1 :: Spec
+propiedadEjecicio1 = describe "EJERCICIO 1.g) Propiedad" $ do
+  it "propiedades de identidad" $ do
+    quickCheck testFrac2ConFrac
 
 -- Pruebas para EJERCICIO 2 =============================
-testsEjecicio1 :: Spec
-testsEjecicio1 = describe "EJERCICIO 1" $ do
---   it "pruebas para primosHastaN" $ do
---     primosHastaN 50 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
---     primosHastaN 10 `shouldBe` [2, 3, 5, 7]
---     primosHastaN 1 `shouldBe` []
---     primosHastaN 2 `shouldBe` [2]
 
-
--- Propiedades para EJERCICIO 2
-propiedadesEjecicio1 :: Spec
-propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
---   it "propiedades de factorizar" $ do
---     quickCheck prop_cantidadFactores
+-- Ejemplo de prueba para una función del ejercicio 2
+testsEjecicio2 :: Spec
+testsEjecicio2 = describe "EJERCICIO 2" $ do
+  -- Añadir pruebas específicas para el ejercicio 2 aquí
+  return ()
 
 -- Pruebas para EJERCICIO 3 =============================
-testsEjecicio1 :: Spec
-testsEjecicio1 = describe "EJERCICIO 1" $ do
---   it "pruebas para primosHastaN" $ do
---     primosHastaN 50 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
---     primosHastaN 10 `shouldBe` [2, 3, 5, 7]
---     primosHastaN 1 `shouldBe` []
---     primosHastaN 2 `shouldBe` [2]
 
-
--- Propiedades para EJERCICIO 3
-propiedadesEjecicio1 :: Spec
-propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
---   it "propiedades de factorizar" $ do
---     quickCheck prop_cantidadFactores
+-- Ejemplo de prueba para una función del ejercicio 3
+testsEjecicio3 :: Spec
+testsEjecicio3 = describe "EJERCICIO 3" $ do
+  -- Añadir pruebas específicas para el ejercicio 3 aquí
+  return ()
 
 -- Pruebas para EJERCICIO 4 =============================
-testsEjecicio1 :: Spec
-testsEjecicio1 = describe "EJERCICIO 1" $ do
---   it "pruebas para primosHastaN" $ do
---     primosHastaN 50 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
---     primosHastaN 10 `shouldBe` [2, 3, 5, 7]
---     primosHastaN 1 `shouldBe` []
---     primosHastaN 2 `shouldBe` [2]
 
-
--- Propiedades para EJERCICIO 4
-propiedadesEjecicio1 :: Spec
-propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
---   it "propiedades de factorizar" $ do
---     quickCheck prop_cantidadFactores
+-- Ejemplo de prueba para una función del ejercicio 4
+testsEjecicio4 :: Spec
+testsEjecicio4 = describe "EJERCICIO 4" $ do
+  -- Añadir pruebas específicas para el ejercicio 4 aquí
+  return ()
 
 -- Pruebas para EJERCICIO 5 =============================
-testsEjecicio1 :: Spec
-testsEjecicio1 = describe "EJERCICIO 1" $ do
---   it "pruebas para primosHastaN" $ do
---     primosHastaN 50 `shouldBe` [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
---     primosHastaN 10 `shouldBe` [2, 3, 5, 7]
---     primosHastaN 1 `shouldBe` []
---     primosHastaN 2 `shouldBe` [2]
 
-
--- Propiedades para EJERCICIO 5
-propiedadesEjecicio1 :: Spec
-propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
---   it "propiedades de factorizar" $ do
---     quickCheck prop_cantidadFactores
-
+-- Ejemplo de prueba para una función del ejercicio 5
+testsEjecicio5 :: Spec
+testsEjecicio5 = describe "EJERCICIO 5" $ do
+  -- Añadir pruebas específicas para el ejercicio 5 aquí
+  return ()
 
 {-------------------------------------------}
 {------------------ MAIN -------------------}
@@ -94,17 +87,13 @@ propiedadesEjecicio1 = describe "EJERCICIO 1 Propiedades" $ do
 
 main :: IO ()
 main = hspec $ do
-  testsEjecicio1
-  propiedadesEjecicio1
-
+  testsEjecicio1ParteB
+  testsEjecicio1ParteC
+  testsEjecicio1ParteD
+  testsEjecicio1ParteE
+  testsEjecicio1ParteF
+  propiedadEjecicio1
   testsEjecicio2
-  propiedadesEjecicio2
-
   testsEjecicio3
-  propiedadesEjecicio3
-
   testsEjecicio4
-  propiedadesEjecicio4
-
   testsEjecicio5
-  propiedadesEjecicio5
--- app/T2.hs
+++ app/T2.hs
@@ -1,116 +1,236 @@
-module Main where
+{-# LANGUAGE TupleSections #-}
 
-  import Test.Hspec
-  import Test.QuickCheck
+module T2 where
 
+{-------------------------------------------}
+{--------------  EJERCICIO 1  --------------}
+{-------------------------------------------}
 
-  {-------------------------------------------}
-  {--------------  EJERCICIO 1  --------------}
-  {-------------------------------------------}
+-- Parte a)
 
-  -- Parte a)
+data ContFraction
+  = Simple Integer
+  | Compound Integer Integer ContFraction
+  deriving (Eq, Show, Ord)
 
+t :: ContFraction
+t = Compound 3 1 (Compound 4 1 (Compound 12 1 (Simple 4)))
 
-  -- Parte b)
-  -- evalCF :: ContFraction -> (Integer,Integer)
+-- Parte b)
+evalCF :: ContFraction -> (Integer, Integer)
+evalCF (Simple a) = (a, 1)
+evalCF (Compound a b frac) = (a * num + b * den, num) -- desarrollo de la formula para que haya una unica fraccion
+  where
+    (num, den) = evalCF frac
 
+-- Parte c)
+degree :: ContFraction -> Integer
+degree (Simple _) = 0
+degree (Compound _ _ frac) = 1 + degree frac
 
-  -- Parte c)
-  -- degree ::  ContFraction -> Integer
+-- Parte d)
+-- por las funciones desarrolladar previamente se puede notar un comportamiendo
+-- diferente cuando se encuentra con un ContFraction Simple o Compound
+-- ( se aplican diferentes funciones)
+foldCF :: ContFraction -> (Integer -> a) -> (Integer -> Integer -> a -> a) -> a
+foldCF (Simple val) fSim _ = fSim val
+foldCF (Compound aval bval frac) fSim fCom =
+  fCom
+    aval
+    bval
+    (foldCF frac fSim fCom)
 
+-- Parte e)
+foldEvalCF :: ContFraction -> (Integer, Integer)
+foldEvalCF frac =
+  foldCF
+    frac
+    (,1)
+    (\a b (num, den) -> (a * num + b * den, num))
 
-  -- Parte d)
+foldDegree :: ContFraction -> Integer
+foldDegree frac =
+  foldCF
+    frac
+    (const 0)
+    (\_ _ gradInt -> 1 + gradInt)
 
+-- Parte f)
+frac2ConFrac :: (Integer, Integer) -> ContFraction
+frac2ConFrac (val, 1) = Simple val
+frac2ConFrac (num, den) = Compound (num `div` den) 1 (frac2ConFrac (den, num `mod` den))
 
-  -- Parte e)
+-- parte g)
+testFrac2ConFrac :: Integer -> Integer -> Bool
+-- se modifico el tipo para correr el test en el archivo main
+testFrac2ConFrac m n
+  | m < 0 = True -- caso no contemplado en la propiedad
+  | n <= 0 = True -- caso no contemplado en la propiedad
+  | otherwise = evalCF (frac2ConFrac (m, n)) == (m, n)
 
+{-------------------------------------------}
+{--------------  EJERCICIO 2  --------------}
+{-------------------------------------------}
 
-  -- Parte f)
-  -- frac2ConFrac :: (Integer, Integer) -> ContFraction
+type Variable = Char
 
+data Formula
+  = Const Bool
+  | Var Variable
+  | Not Formula
+  | And Formula Formula
+  | Imply Formula Formula
 
-  -- parte g)
-  -- testFrac2ConFrac :: Spec
+-- Parte (a)
+foldF ::
+  (Bool -> a) -> -- funcion para Const
+  (Variable -> a) -> -- Funcion para Var
+  (a -> a) -> -- Funcion para Not
+  (a -> a -> a) -> -- Funcion para And
+  (a -> a -> a) -> -- Funcion para Imply
+  Formula ->
+  a -- Finalmente la formula y retorna algo tipo a
+foldF fConst fVar fNot fAnd fImply formula = case formula of
+  Const p -> fConst p
+  Var p -> fVar p
+  Not p -> fNot (foldF fConst fVar fNot fAnd fImply p)
+  And p q -> fAnd (foldF fConst fVar fNot fAnd fImply p) (foldF fConst fVar fNot fAnd fImply q)
+  Imply p q -> fImply (foldF fConst fVar fNot fAnd fImply p) (foldF fConst fVar fNot fAnd fImply q)
 
+-- Parte (b)
+foldEvalF :: -- se cambio el nombre de fold a foldEvalF para evitar colision de nombre
+  Formula ->
+  Valuation ->
+  Bool -- retorna Bool
+foldEvalF formula s =
+  foldF
+    id
+    ( \x -> case find x s of
+        Right val -> val
+        Left val -> error val
+    )
+    not
+    (&&)
+    (<=)
+    formula
 
+fvar ::
+  Formula -> [Char]
+fvar =
+  foldF
+    (const [])
+    (: [])
+    id
+    (++)
+    (++)
 
+-- Parte (c)
+rmdups :: (Eq a) => [a] -> [a]
+rmdups [] = []
+rmdups (x : xs) = x : rmdups (filter (/= x) xs)
 
-  {-------------------------------------------}
-  {--------------  EJERCICIO 2  --------------}
-  {-------------------------------------------}
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False :) r ++ map (True :) r
+  where
+    r = bools (n - 1)
 
-  type Variable  = Char
-  data Formula   = Const Bool
-                 | Var Variable
-                 | Not Formula
-                 | And Formula Formula
-                 | Imply Formula Formula
-
-
-
-  -- Parte (a)
-
-
-  -- Parte (b)
-
-
-  -- Parte (c)
-  rmdups :: Eq a => [a] -> [a]
-  rmdups [] = []
-  rmdups (x:xs) = x : rmdups (filter (/= x) xs)
-
-  bools :: Int -> [[Bool]]
-  bools 0 = [[]]
-  bools n = map (False :) r ++ map (True :) r where
-    r = bools (n-1)
-
-  allVals :: Formula -> [Valuation]
-  allVals f = map (zip vars) vals where
+allVals :: Formula -> [Valuation]
+allVals f = map (zip vars) vals
+  where
     vars = rmdups (fvar f)
     vals = bools (length vars)
 
-  -- isTaut :: Formula -> Maybe Valuation
+isTaut :: Formula -> Maybe Valuation
+isTaut formula =
+  if and trueValues
+    then Nothing
+    else eval1 formula valuations
+  where
+    valuations = allVals formula
+    trueValues = map (foldEvalF formula) valuations
+
+    -- eval1: entrega la primera valuacion que haga la formula False
+    eval1 _ [] = Nothing
+    eval1 form (val : vals)
+      | not (foldEvalF form val) = Just val
+      | otherwise = eval1 form vals
+
+{-------------------------------------------}
+{--------------  EJERCICIO 3  --------------}
+{-------------------------------------------}
+
+type Assoc k v = [(k, v)]
+
+type Error = String
+
+type Valuation = Assoc Char Bool
+
+-- Parte (a)
+find ::
+  (Eq k, Show k, Eq v) =>
+  k ->
+  Assoc k v ->
+  Either Error v
+find key assoc = case [v | (k, v) <- assoc, k == key] of
+  [] -> Left ( "Key " ++ show key ++ " not found")
+  [v] -> Right v
+  _ -> Left ("Multiple values for key " ++ show key)
+
+-- Parte (b)
+-- Eq k: Se debe declarar que k es de tipo Eq para comparar igualdad con la llave buscada.
+-- Show k: Se necesita que k cumpla este tipo para poder ser mostrado en pantalla con el mensaje de error.
+-- Eq v: Se necesita comparar igualdad también en v para revisar si existen valores duplicados (iguales) asociados a la misma clave k.
+
+{-------------------------------------------}
+{--------------  EJERCICIO 4  --------------}
+{-------------------------------------------}
+
+data Nat = Zero | Succ Nat deriving (Show, Eq)
+
+add :: Nat -> Nat -> Nat
+add Zero m = m
+add (Succ n) m = Succ (add n m)
+
+mult :: Nat -> Nat -> Nat
+mult Zero _ = Zero
+mult (Succ n) m = add m (mult n m)
+
+foldNat :: (b -> b) -> b -> Nat -> b
+foldNat _ v Zero = v
+foldNat f v (Succ n) = f (foldNat f v n)
+
+-- se agrega funcion para restar
+subs :: Nat -> Nat -> Nat
+subs Zero _ = Zero
+subs n Zero = n
+subs (Succ n) (Succ m) = subs n m
+
+-- numeros que se repite uso
+uno :: Nat
+uno = Succ Zero
+
+dos :: Nat
+dos = Succ uno
+
+-- funcion aux para obtener una lista de los primeros n numeros pares
+nFirstEven :: Nat -> [Nat]
+nFirstEven = genList Zero
+  where
+    genList :: Nat -> Nat -> [Nat]
+    genList _ Zero = []
+    genList count (Succ k) = mult dos count : genList (Succ count) k
 
 
+fun :: Nat -> Nat
+fun n = foldNat funN dos n
+  where
+    funN :: Nat -> Nat
+    funN k 
+      | k `elem` nFirstEven n = add k (fun (subs n dos))
+      | otherwise = add (mult k k) (fun (subs n uno))
 
 
-  {-------------------------------------------}
-  {--------------  EJERCICIO 3  --------------}
-  {-------------------------------------------}
-
-  type Assoc k v = [(k,v)]
-  type Error     = String
-
-  -- Parte (a)
-
-
-  -- Parte (b)
-
-
-
-  {-------------------------------------------}
-  {--------------  EJERCICIO 4  --------------}
-  {-------------------------------------------}
-
-  data Nat = Zero | Succ Nat deriving Show
-
-  add :: Nat -> Nat -> Nat
-  add Zero     m = m
-  add (Succ n) m = Succ (add n m)
-
-  mult :: Nat -> Nat -> Nat
-  mult Zero     m = Zero
-  mult (Succ n) m = add m (mult n m)
-
-  foldNat :: (b -> b) -> b -> Nat -> b
-  foldNat f v Zero     = v
-  foldNat f v (Succ n) = f (foldNat f v n)
-
-
-
-
-
-  {-------------------------------------------}
-  {--------------  EJERCICIO 5  --------------}
-  {-------------------------------------------}
-
+{-------------------------------------------}
+{--------------  EJERCICIO 5  --------------}
+{-------------------------------------------}
